be597da3116bf8a3058a49c8eb22d617e9d5806b
SAM PELOT <sampelot93@gmail.com>
2026-09-20 18:21:59 -0400

    Reading from the StudentData file. Printing out just for visual validation.

---
+++ Proj5_Asn2/main.cpp
@@ -0,0 +1,53 @@
+// Sam - Septembre 20 2026
+
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <string>
+#include <fstream>
+
+
+using namespace std;
+
+#define STUDENT_FILE	"StudentData.txt"
+#define STUDENT_EMAILS	"StudentdData_Emails.txt"
+
+typedef struct student
+{
+	string firstName;
+	string lastName;
+}STUDENT;
+
+int main()
+{
+	STUDENT student;
+	string line;
+
+	vector<STUDENT> students;
+
+	ifstream StudentFile(STUDENT_FILE);
+	if (!StudentFile.is_open())
+	{
+		cout << "Error opening file: " << STUDENT_FILE << endl;
+		return 1;
+	}
+	else 
+	{
+		while (getline(StudentFile, line))
+		{
+			stringstream ss(line);
+			getline(ss, student.firstName, ',');
+			getline(ss, student.lastName);
+			//cout << "First Name: " << s1.firstName << ", Last Name: " << s1.lastName << endl;
+			students.push_back(student);
+		}
+	}
+
+	for (const STUDENT& s : students)
+	{
+		cout << "First Name: " << s.firstName << ", Last Name: " << s.lastName << endl;
+	}
+
+
+	return 0;
+}
